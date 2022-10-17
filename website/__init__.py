e9bc50ce29cd934afe422072156cbc55c8ab1bfc
Jacques Roux <jacquesf.roux@gmail.com>
2022-10-17 17:01:14 +0200

    Adding DB & models

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
   app = Flask(__name__)
   app.config['SECRET_KEY'] = 'kjashdkjsahdkjsahdkasjhdskajhdksajh'
+  app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+  db.init_app(app)
 
   from .views import views
   from .auth import auth
@@ -10,4 +16,10 @@ def create_app():
   app.register_blueprint(views, url_prefix='/')
   app.register_blueprint(auth, url_prefix='/')
 
+  from . import models
+
+  with app.app_context():
+    db.create_all()
+
   return app
+
