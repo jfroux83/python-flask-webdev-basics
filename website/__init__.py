bd543f3b67cd37a3d9222f29654ebc4bae5bd430
Jacques Roux <jacquesf.roux@gmail.com>
2022-10-19 16:58:39 +0200

    final code for project adding notes

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -16,10 +17,18 @@ def create_app():
   app.register_blueprint(views, url_prefix='/')
   app.register_blueprint(auth, url_prefix='/')
 
-  from . import models
+  from .models import User, Note
 
   with app.app_context():
     db.create_all()
+
+  login_manager = LoginManager()
+  login_manager.login_view = 'auth.login'
+  login_manager.init_app(app)
+
+  @login_manager.user_loader
+  def load_user(id):
+    return User.query.get(int(id))
 
   return app
 
